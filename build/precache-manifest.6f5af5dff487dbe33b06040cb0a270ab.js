035e674dc8214740079d6e65b23b2b272828437f
PavlosKon <pavlos.kon@outlook.com>
2020-07-06 23:21:25 +0300

    initial commit

---
+++ build/precache-manifest.6f5af5dff487dbe33b06040cb0a270ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c6e7edf3718e1a98561a59538f34e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bd316a25ae1d4fc99a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11235602ea0c1ce0a0da",
+    "url": "/static/js/2.04e7d5bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04e7d5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bd316a25ae1d4fc99a",
+    "url": "/static/js/main.d5cca3d5.chunk.js"
+  },
+  {
+    "revision": "17553778e02af952c207",
+    "url": "/static/js/runtime-main.455471ad.js"
+  }
+]);
